9c4a48f87df608e47baeaed9231736715f5e93d4
Miklos Toth <mklstth@gmail.com>
2024-01-12 23:24:41 +0100

    greeting

--- src/main/java/com/practice/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/practice/springbootexample/SpringBootExampleApplication.java
@@ -2,8 +2,15 @@ package com.practice.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+/*
+* @ComponentScan(basePackage = "com.practice.springbootexample")
+* @Configuration
+* @EnableAutoConfiguration
+* */
 @RestController
 public class SpringBootExampleApplication {
 
