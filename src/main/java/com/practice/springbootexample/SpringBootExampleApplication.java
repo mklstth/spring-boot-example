2f19e1cb7b02eb5a2cf287ab5965bc08614ad90f
Miklos Toth <mklstth@gmail.com>
2024-01-12 22:49:13 +0100

    adding a greeting

--- src/main/java/com/practice/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/practice/springbootexample/SpringBootExampleApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String greet(){
+		return "Hello";
+	}
 }
